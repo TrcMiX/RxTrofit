de540afa31195b0636e070c5c4a5a8aac49a6d3c
曳光剂 <1242067286@qq.com>
2018-03-24 21:21:20 +0800

    03-24

--- app/src/main/java/com/dpk/rxtrofit/MainActivity.kt
+++ app/src/main/java/com/dpk/rxtrofit/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.dpk.rxtrofit
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
